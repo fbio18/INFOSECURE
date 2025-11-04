4e6ce5decae1458aedc1e2f23ccb11e2493c3f72
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-03 23:16:28 -0300

    chore: Inicializando las entidades

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,17 @@
 import { DataSource } from "typeorm";
 import { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_PORT, DB_NAME } from "./db-config";
-
+import User from "./entities/User";
+import Cart from "./entities/Cart";
+import Cart_Product from "./entities/Cart_Product";
+import Client from "./entities/Client";
+import Product from "./entities/Product";
+import Invoice from "./entities/Invoice";
+import Invoice_type from "./entities/Invoice_type";
+import Invoice_Product from "./entities/Invoice_product";
+import Role from "./entities/Role";
+import Nationality from "./entities/Nationality";
+import Employee from "./entities/Employee";
+import Receptor_type from "./entities/Receptor_type";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -13,7 +24,7 @@ export const AppDataSource = new DataSource({
     password: DB_PASSWORD,
     port: (<number>DB_PORT),
     database: DB_NAME,
-    entities: [],
+    entities: [User, Cart, Client, Product, Invoice, Invoice_type, Role, Nationality, Employee, Invoice_Product, Receptor_type, Cart_Product],
     logging: true,
     synchronize: true,
     dropSchema: true
