afaba47901afc13d6e1f8cc632c7b7c2d5313c5f
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-05 17:22:20 -0300

    feat: Añadiendo controlador para crear empleado

--- backend/src/controllers/employee.controllers.ts
+++ backend/src/controllers/employee.controllers.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
+import { validateBody } from "../services/validation";
+import { InvalidBody } from "../services/errorMessages";
+import { createEmployeeService } from "../services/employee.services";
 
 export async function createEmployeeController(req: Request, res: Response) {
+    try {
+        if (!validateBody(req.body)) throw new InvalidBody();
 
+        const employee = await createEmployeeService(req.body);
+
+        res.send(employee);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export async function readEmployeeController(req: Request, res: Response) {
