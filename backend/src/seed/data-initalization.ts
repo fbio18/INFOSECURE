4d37dc83b2ee879e0554be29a9f10811c8e80ba0
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-05 02:30:59 -0300

    fix: cuelgue del servidor cuando no cargan los países

--- backend/src/seed/data-initalization.ts
+++ backend/src/seed/data-initalization.ts
@@ -2,13 +2,17 @@ import NationalityRepository from "../repositories/nationality.repository";
 import { getCountries } from "@yusifaliyevpro/countries";
 
 export async function preloadData() {
-    const countries = await getCountries({ fields: ["name"]});
-    if (!countries) throw new Error();
+    try {
+        const countries = await getCountries({ fields: ["name"]});
+        if (!countries) throw new Error();
 
-    for (const country of countries) {
-        await NationalityRepository.save([
-            { name: country.name.common }
-        ]);
+        for (const country of countries) {
+            await NationalityRepository.save([
+                { name: country.name.common }
+            ]);
+        }
+        console.log("Países cargados en bases de datos");
+    } catch (error) {
+        console.log("No se pudo cargar la lista de países en BD");
     }
-    console.log("Países cargados en bases de datos");
 }
