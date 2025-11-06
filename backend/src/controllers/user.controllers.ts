70e3602aff7e10f144f2c202927073d0d9c3cd42
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-05 22:14:48 -0300

    feat: Añadiendo controladores para lectura de usuarios
    Ahora pueden llegar peticiones de los endpoints /user y /user/:id, para
    solicitar todos los usuarios y para solicitar un solo usuario,
    respectivamente

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
-import { createUserService, readUserService, readsAllUserService, updateUserService, deleteUserService } from "../services/user.services";
+import { createUserService, readUserService, readsAllUsersService, updateUserService, deleteUserService } from "../services/user.services";
 import { validateBody } from "../services/validation";
-import { InvalidBody } from "../services/errorMessages";
+import { InvalidBody, MissingData } from "../services/errorMessages";
 
 export async function createUserController(req: Request, res: Response) {
     try {
@@ -17,7 +17,27 @@ export async function createUserController(req: Request, res: Response) {
 }
 
 export async function readUserController(req: Request, res: Response) {
+    try {
+        if (!req.params.id) throw new MissingData();
 
+        const userId = parseInt(req.params.id);
+
+        const user = await readUserService(userId);
+        
+        res.send(user);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function readAllUsersController(req: Request, res: Response) {
+    try {
+        const users = await readsAllUsersService();
+
+        res.send(users);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export async function updateUserController(req: Request, res: Response) {
