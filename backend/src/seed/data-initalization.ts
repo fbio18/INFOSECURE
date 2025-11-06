e197db34c3f010ae1985c247e85e830560ccb4a0
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-05 22:32:47 -0300

    chore: Agregando datos de usuarios para facilitar testeo

--- backend/src/seed/data-initalization.ts
+++ backend/src/seed/data-initalization.ts
@@ -1,4 +1,5 @@
 import NationalityRepository from "../repositories/nationality.repository";
+import UserRepository from "../repositories/user.repository";
 import { getCountries } from "@yusifaliyevpro/countries";
 
 export async function preloadData() {
@@ -11,6 +12,29 @@ export async function preloadData() {
                 { name: country.name.common }
             ]);
         }
+
+        await UserRepository.save([
+            {
+                username: "Nico",
+                email: "miqueasnruarte@protonmail.com",
+                password: "12345678"
+            },
+            {
+                username: "BitsHunter",
+                email: "pabloHunter@gmail.com",
+                password: "holasoygay"
+            },
+            {
+                username: "ZackDaga",
+                email: "zackdaga@gmail.com",
+                password: "soyunenano+10"
+            },
+            {
+                username: "Saparrafpa",
+                email: "chad67depenopochepe",
+                password: "4khd7822"
+            }
+        ]);
         console.log("Países cargados en bases de datos");
     } catch (error) {
         console.log("No se pudo cargar la lista de países en BD");
