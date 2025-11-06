ce15223f701a89e3f079dd1b4b9b8d03cdcf8349
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-06 00:56:48 -0300

    feat: Agregando controlador para crear cliente

--- backend/src/controllers/client.controllers.ts
+++ backend/src/controllers/client.controllers.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
+import { validateBody } from "../services/validation";
+import { MissingData } from "../services/errorMessages";
+import { createClientService } from "../services/client.services";
 
 export async function createClientController(req: Request, res: Response) {
+    try {
+        if (!validateBody(req.body)) throw new MissingData();
 
+        const client = await createClientService();
+
+        res.send(client);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export async function readClientController(req: Request, res: Response) {
