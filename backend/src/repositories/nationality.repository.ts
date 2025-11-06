931ac03e43d2157d25495b43f1f91bae4983ecf3
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-06 00:58:14 -0300

    feat: Agregando funcionalidad de leer una nacionalidad

--- backend/src/repositories/nationality.repository.ts
+++ backend/src/repositories/nationality.repository.ts
@@ -1,6 +1,18 @@
 import AppDataSource from "../db";
 import Nationality from "../entities/Nationality";
+import { NotFound } from "../services/errorMessages";
 
-const NationalityRepository = AppDataSource.getRepository(Nationality);
+const NationalityRepository = AppDataSource.getRepository(Nationality).extend({
+    async readNationality(nationalityId: number) {
+        const nationality = await this
+        .createQueryBuilder("nationality")
+        .where("nationality.nationality_id = :nationalityId", { nationalityId })
+        .getOne();
+
+        if (!nationality) throw new NotFound("nationality");
+
+        return nationality;
+    },
+});
 
 export default NationalityRepository;
