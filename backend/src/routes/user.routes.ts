2c2a6391373a93550b53e303ffcda9623989772e
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-05 22:32:04 -0300

    feat: Agregando rutas para seleccionar por id

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createUserController, readUserController, updateUserController, deleteUserController } from "../controllers/user.controllers";
+import { createUserController, readUserController, updateUserController, deleteUserController, readAllUsersController } from "../controllers/user.controllers";
 
 const router = Router();
 
 router.post("/user", createUserController);
-router.get("/user", readUserController);
+router.get("/user/:id", readUserController);
+router.get("/user", readAllUsersController);
 router.put("/user", updateUserController);
 router.delete("/user", deleteUserController);
 
