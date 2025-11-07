e4b6ca7ddde55196ed2f178687093af15c482558
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-07 14:53:29 -0300

    feat: Agregando controladores para lectura de productos
    Los controladores son para leer producto mediante o para listar todos
    los productos de la base de datos

--- backend/src/controllers/product.controllers.ts
+++ backend/src/controllers/product.controllers.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { validateBody } from "../services/validation";
-import { InvalidBody } from "../services/errorMessages";
-import { createProductService } from "../services/product.services";
+import { createErrorResponse, InvalidBody, MissingData } from "../services/errorMessages";
+import { createProductService, readAllProductsService, readProductService } from "../services/product.services";
 
 export async function createProductController(req: Request, res: Response) {
     try {
@@ -17,7 +17,28 @@ export async function createProductController(req: Request, res: Response) {
 }
 
 export async function readProductController(req: Request, res: Response) {
+    try {
+        if (!req.params.id) throw new MissingData();
+        const id = parseInt(req.params.id);
 
+        const product = await readProductService(id);
+
+        res.send(product);
+    } catch (error) {
+        console.error(error);
+        res.send(createErrorResponse(error));
+    }
+}
+
+export async function readAllProductsController(req: Request, res: Response) {
+    try {
+        const products = await readAllProductsService();
+        
+        res.send(products);
+    } catch (error) {
+        console.error(error);
+        res.send(createErrorResponse(error));
+    }
 }
 
 export async function updateProductController(req: Request, res: Response) {
