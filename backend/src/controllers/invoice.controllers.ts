390316b7746a571c5562bd8db9de7b5bb7fde9c0
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-06 22:15:48 -0300

    feat: Añadiendo controlador de creación de invoices

--- backend/src/controllers/invoice.controllers.ts
+++ backend/src/controllers/invoice.controllers.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from "express";
+import { validateBody } from "../services/validation";
+import { createErrorResponse, InvalidBody } from "../services/errorMessages";
+import { createInvoiceService } from "../services/invoice.services";
 
 export async function createInvoiceController(req: Request, res: Response) {
+    try {
+        if (!validateBody(req.body)) throw new InvalidBody();
 
+        const invoice = await createInvoiceService(req.body);
+        
+        res.send(invoice);
+    } catch (error) {
+        console.error(error);
+        res.send(createErrorResponse(error));
+    }
 }
 
 export async function readInvoiceController(req: Request, res: Response) {
