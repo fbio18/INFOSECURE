0584b05bf8569ec552d09323ed0cde2d868893c5
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-07 20:28:23 -0300

    fix: Cambiando de lugar los atributos
    Para seguir la regla de que los atributos que sean relaciones con otras
    entidades deben estar al fondo

--- backend/src/entities/Invoice.ts
+++ backend/src/entities/Invoice.ts
@@ -18,12 +18,12 @@ export default class Invoice extends BaseEntity {
     @Column()
     total_amount: number;
 
-    @OneToMany(() => Client, (client) => client.invoices)
-    client: Client;
-
     @Column({ length: 1 })
     invoice_type: TInvoice_type;
 
+    @OneToMany(() => Client, (client) => client.invoices)
+    client: Client;
+
     @OneToMany(() => Invoice_Product, (invoice_product) => invoice_product.invoice)
-    invoice_products: Invoice_Product[];
+    products: Invoice_Product[];
 }
