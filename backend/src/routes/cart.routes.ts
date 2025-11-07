7fe5a144b0f2341e7779f4db5cbedfe7fcc44aee
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-07 20:42:29 -0300

    fix: Agregando función de controlador faltante

--- backend/src/routes/cart.routes.ts
+++ backend/src/routes/cart.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createCartController, readCartController, updateCartController, deleteCartController } from "../controllers/cart.controllers";
+import { createCartController, readCartController, updateCartController, deleteCartController, readAllCartsController } from "../controllers/cart.controllers";
 
 const router = Router();
 
 router.post("/cart", createCartController);
 router.get("/cart/:id", readCartController)
-router.get("/cart")
+router.get("/cart", readAllCartsController);
 router.put("/cart", updateCartController)
 router.delete("/cart", deleteCartController)
 
