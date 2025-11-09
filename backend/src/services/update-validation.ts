2f19dc907aaddb9083f5cf942cb3969ce8567723
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-11-09 01:28:06 -0300

    feat: Agregando validación para los datos de actualización de usuario

--- backend/src/services/update-validation.ts
+++ backend/src/services/update-validation.ts
@@ -1,5 +1,29 @@
 import * as v from "valibot";
-import { messages, validateNumericString } from "./validation";
+import { messages, MINPASSWORDLENGTH, validateNumericString } from "./validation";
+
+const updateUserSchema = v.object({
+    email: v.nullish(
+        v.pipe(
+            v.string(messages.string),
+            v.email(),
+            v.nonEmpty(messages.nonEmpty),
+    )),
+    password: v.nullish(
+        v.pipe(
+            v.string(messages.string),
+            v.nonEmpty(messages.nonEmpty),
+            v.minLength(MINPASSWORDLENGTH)
+        )),
+    username: v.nullish(
+        v.pipe(
+            v.string(messages.string),
+            v.nonEmpty(messages.nonEmpty),
+    ))
+});
+export type UpdateUserValidated = v.InferOutput<typeof updateUserSchema>;
+export function validateUpdateUserData(userData: unknown) {
+    return v.parse(updateUserSchema, userData);
+}
 
 const updateClientSchema = v.object({
     business_name: v.nullish(
